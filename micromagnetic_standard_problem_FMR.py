4be7ae118d62260ed5ef648bba0c405826436a53
mb4e10 <mb4e10@soton.ac.uk>
2015-07-30 21:02:34 +0100

    Add matplotlib Agg.

--- micromagnetic_standard_problem_FMR.py
+++ micromagnetic_standard_problem_FMR.py
@@ -1,6 +1,7 @@
 """ Collection of all the common tools used in the project """
 import os
 import numpy as np
+import matplotlib; matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import argparse
 
