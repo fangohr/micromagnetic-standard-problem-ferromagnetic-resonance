a07dfee63aa9224543e5d552a8412b3bc9890351
mb4e10 <mb4e10@soton.ac.uk>
2015-07-31 15:52:58 +0100

    Rename, comment, tidy up. Consistent with paper. Remove camel notation.

--- Nmag_standard_problem/dynamic_stage.py
+++ Nmag_standard_problem/dynamic_stage.py
@@ -1,20 +1,30 @@
 import nmag
 from nmag import SI, every, at
 
-h_Strength = 80e3  # A/m
+mesh_name = 'mesh_24.nmesh.h5'
+relax_name = 'relax'
 
-alpha = 0.008
-meshName = 'mesh_24' + '.nmesh.h5'
-relaxName = 'Relax'
+# Material parameters
+Ms = 8e5  # saturation magnetisation (A/m)
+A = 13e-12  # exchange energy constant (J/m)
+gamma = 2.210173e5  # gyromagnetic ratio (m/As)
+alpha = 0.008  # Gilbert damping
 
-xField = 0.819152044289
-yField = 0.573576436351
+# External magnetic field.
+H = 80e3  # A/m
+x_direction = 0.819152044289
+y_direction = 0.573576436351
+z_direction = 0
+
+# Sampling parameters
+dt = 5e-12  # time step (s)
+T = 20e-9  # total simulation time (s)
 
 # Define magnetic material: PermAlloy
 Py = nmag.MagMaterial(name="Py",
-                      Ms=SI(8e5, 'A/m'),
-                      exchange_coupling=SI(13e-12, "J/m"),
-                      llg_gamma_G=SI(2.210173e5, 'm/A s'),
+                      Ms=SI(Ms, 'A/m'),
+                      exchange_coupling=SI(A, "J/m"),
+                      llg_gamma_G=SI(gamma, 'm/A s'),
                       llg_damping=alpha)
 
 
@@ -22,18 +32,17 @@ Py = nmag.MagMaterial(name="Py",
 sim = nmag.Simulation()
 
 # Load the mesh
-sim.load_mesh(meshName, [("Permalloy", Py)], unit_length=SI(1e-9, "m"))
+sim.load_mesh(mesh_name, [("Permalloy", Py)], unit_length=SI(1e-9, "m"))
 
 # Load the initial magnetisation
-sim.load_m_from_h5file(relaxName + ".h5")
+sim.load_m_from_h5file(relax_name + ".h5")
 
 # Set the applied magnetic field
-sim.set_H_ext([xField, yField, 0], SI(h_Strength, 'A/m'))
+sim.set_H_ext([x_direction, y_direction, z_direction], SI(H, 'A/m'))
 
 # Set convergence parameters
 sim.set_params(stopping_dm_dt=0.0)
 
-# Save the information ever 5ps, and exit after 20ns. Note that we will never
-# meet the convergence parameter that we have set, so we hard exit
-sim.relax(save=[('fields', every('time', SI(5.0e-12, "s")))],
-          do=[('exit', at('time', SI(20000*1e-12, "s")))])
+# Save the information ever 5ps, and exit after 20ns.
+sim.relax(save=[('fields', every('time', SI(dt, "s")))],
+          do=[('exit', at('time', SI(T, "s")))])
