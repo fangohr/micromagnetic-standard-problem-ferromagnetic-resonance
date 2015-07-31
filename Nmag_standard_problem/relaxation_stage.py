6d521cc59fb6b2a80b5bb345e49570107cc0be79
mb4e10 <mb4e10@soton.ac.uk>
2015-07-31 15:53:14 +0100

    Rename, comment, tidy up. Consistent with paper. Remove camel notation.

--- Nmag_standard_problem/relaxation_stage.py
+++ Nmag_standard_problem/relaxation_stage.py
@@ -1,43 +1,49 @@
 import nmag
 from nmag import SI, at
 
-mSat = 0.8e6
-exchange_Strength = 13e-12
-meshName = 'mesh_24' + '.nmesh.h5'
+mesh_name = 'mesh_24.nmesh.h5'
+sim_output_name = 'relax'
 
-xField = 0.813405448449
-yField = 0.581697151818
-h_Strength = 80e3  # A/m
+# Material parameters
+Ms = 8e5  # saturation magnetisation (A/m)
+A = 13e-12  # exchange energy constant (J/m)
+gamma = 2.210173e5  # gyromagnetic ratio (m/As)
+alpha = 1  # Gilbert damping
 
-stoppingTime = 5000
+# External magnetic field.
+H = 80e3  # A/m
+x_direction = 0.813405448449
+y_direction = 0.581697151818
+z_direction = 0
 
-simOutputName = 'Relax'
+# Total simulation time
+T = 5e-9
 
 # Define magnetic material: PermAlloy
 Py = nmag.MagMaterial(name="Py",
-                      Ms=SI(mSat, 'A/m'),
-                      exchange_coupling=SI(exchange_Strength, "J/m"),
-                      llg_gamma_G=SI(2.210173e5, 'm/A s'),
-                      llg_damping=1.0)
+                      Ms=SI(Ms, 'A/m'),
+                      exchange_coupling=SI(A, "J/m"),
+                      llg_gamma_G=SI(gamma, 'm/A s'),
+                      llg_damping=alpha)
 
 # Create the simulation object
 sim = nmag.Simulation()
 
 # Load the mesh
-sim.load_mesh(meshName, [("Permalloy", Py)], unit_length=SI(1e-9, "m"))
+sim.load_mesh(mesh_name, [("Permalloy", Py)], unit_length=SI(1e-9, "m"))
 
-# Set initial magnetization
+# Set initial magnetization state in positive z direction
 sim.set_m([0.0, 0.0, 1.0])
 
 # And set the external field
-sim.set_H_ext([xField, yField, 0], SI(h_Strength, 'A/m'))
+sim.set_H_ext([x_direction, y_direction, z_direction], SI(H, 'A/m'))
 
 # Set convergence parameters
 sim.set_params(stopping_dm_dt=0)
 
 # Save all the information out at the start and at the end
 sim.relax(save=[('fields', at('stage_end'))],
-          do=[('exit', at('time', SI(stoppingTime*1e-12, "s")))])
+          do=[('exit', at('time', SI(T, "s")))])
 
 # Finally, save the system state for the dynamic stage to run from
-sim.save_restart_file(simOutputName + '.h5')
+sim.save_restart_file(sim_output_name + '.h5')
