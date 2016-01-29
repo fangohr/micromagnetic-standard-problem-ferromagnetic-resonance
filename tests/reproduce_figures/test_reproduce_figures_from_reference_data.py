2a1f01d63f93d29ffc111994864b3c454b4ebdf6
Maximilian Albert <maximilian.albert@gmail.com>
2016-01-29 13:46:46 +0000

    Add tests to reproduce Figures 2 and 3 from reference data.

---
+++ tests/reproduce_figures/test_reproduce_figures_from_reference_data.py
@@ -0,0 +1,33 @@
+from matplotlib.testing.decorators import image_comparison
+from pathlib import Path
+
+from postprocessing import DataReader, make_figure_2, make_figure_3, make_figure_4
+
+TOL = 0
+
+# Get absolute path to the current directory (to avoid problems if
+# this script is invoked from somewhere else).
+here = Path(__file__).parent.resolve()
+
+#
+# Define input and output directories.
+#
+REF_DATA_DIR_OOMMF = here.joinpath('../../micromagnetic_simulation_data/reference_data/oommf/')
+REF_DATA_DIR_NMAG = here.joinpath('../../micromagnetic_simulation_data/reference_data/nmag/')
+
+#
+# Create SimulationReader which provides a convenient way of
+# reading raw simulation data and computing derived data.
+#
+data_reader_oommf = DataReader(REF_DATA_DIR_OOMMF, data_format='OOMMF')
+data_reader_nmag = DataReader(REF_DATA_DIR_NMAG, data_format='Nmag')
+
+
+@image_comparison(baseline_images=['figure_2_OOMMF'], extensions=['png', 'pdf'], tol=TOL)
+def test__reproduce_figure_2():
+    fig = make_figure_2(data_reader_oommf)
+
+
+@image_comparison(baseline_images=['figure_3_OOMMF'], extensions=['png', 'pdf'], tol=TOL)
+def test__reproduce_figure_3():
+    fig = make_figure_3(data_reader_oommf)
