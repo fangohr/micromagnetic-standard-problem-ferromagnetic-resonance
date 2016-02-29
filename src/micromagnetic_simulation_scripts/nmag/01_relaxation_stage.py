cb1770adf3e761458a5b980f902c4481e5d3168f
Maximilian Albert <maximilian.albert@gmail.com>
2016-02-29 17:15:56 +0000

    Specify H_ext in human-readable coordinates (using the values specified in the paper) and use numpy to normalise the direction.

--- src/micromagnetic_simulation_scripts/nmag/01_relaxation_stage.py
+++ src/micromagnetic_simulation_scripts/nmag/01_relaxation_stage.py
@@ -1,3 +1,4 @@
+import numpy as np
 import nmag
 from nmag import SI, at
 
@@ -12,9 +13,8 @@ alpha = 1  # Gilbert damping
 
 # External magnetic field.
 H = 80e3  # A/m
-x_direction = 0.81345856316858023
-y_direction = 0.58162287266553481
-z_direction = 0.0
+H_direction = np.array([1.0, 0.715, 0.0])
+H_direction_normalized = H_direction / np.linalg.norm(H_direction)
 
 # Total simulation time
 T = 5e-9
@@ -36,7 +36,7 @@ sim.load_mesh(mesh_name, [("Permalloy", Py)], unit_length=SI(1e-9, "m"))
 sim.set_m([0.0, 0.0, 1.0])
 
 # And set the external field
-sim.set_H_ext([x_direction, y_direction, z_direction], SI(H, 'A/m'))
+sim.set_H_ext([H_direction_normalized], SI(H, 'A/m'))
 
 # Set convergence parameters
 sim.set_params(stopping_dm_dt=0)
--- src/micromagnetic_simulation_scripts/nmag/02_dynamic_stage.py
+++ src/micromagnetic_simulation_scripts/nmag/02_dynamic_stage.py
@@ -1,3 +1,4 @@
+import numpy as np
 import nmag
 from nmag import SI, every, at
 
@@ -12,9 +13,8 @@ alpha = 0.008  # Gilbert damping
 
 # External magnetic field.
 H = 80e3  # A/m
-x_direction = 0.81923192051904048
-y_direction = 0.57346234436332832
-z_direction = 0
+H_direction = np.array([1.0, 0.715, 0.0])
+H_direction_normalized = H_direction / np.linalg.norm(H_direction)
 
 # Sampling parameters
 dt = 5e-12  # time step (s)
@@ -48,7 +48,7 @@ sim.load_mesh(mesh_name, [("Permalloy", Py)], unit_length=SI(1e-9, "m"))
 sim.load_m_from_h5file(relax_name + ".h5")
 
 # Set the applied magnetic field
-sim.set_H_ext([x_direction, y_direction, z_direction], SI(H, 'A/m'))
+sim.set_H_ext([H_direction_normalized], SI(H, 'A/m'))
 
 # Set convergence parameters
 sim.set_params(stopping_dm_dt=0.0, ts_abs_tol=1e-7, ts_rel_tol=1e-7)
